2a4ce2c203502af25cb3140c7056bce9afae84e8
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-27 22:29:25 +0200

    icons added

--- src/components/Categories/icons.jsx
+++ src/components/Categories/icons.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { MdOutlinePhonelink, MdOutlineMapsHomeWork, MdMapsHomeWork, MdOutlineToys, MdEmojiFoodBeverage } from "react-icons/md";
+import { AiOutlineAudio } from "react-icons/ai";
+import { MdOutlinePhonelink, MdOutlineMapsHomeWork, MdMapsHomeWork, MdOutlineToys, MdEmojiFoodBeverage, MdPhoneIphone, MdOutlineLaptopChromebook, MdTabletMac, MdOutlineDesktopWindows } from "react-icons/md";
 import { FaBookDead, FaCar } from "react-icons/fa";
+import { FaComputer } from "react-icons/fa6";
 import { GiClothes } from "react-icons/gi";
-import { PiPottedPlantBold, PiHammerBold, PiLampPendantBold } from "react-icons/pi";
+import { PiPottedPlantBold, PiHammerBold, PiLampPendantBold, PiForkKnifeBold, PiBowlSteamBold, PiBroomFill, PiBathtubBold, PiTelevisionSimpleFill, PiVideoCameraBold } from "react-icons/pi";
+import { RiScales2Line } from "react-icons/ri";
 import { SiGumtree } from "react-icons/si";
-import { TbPoint, TbSofa } from "react-icons/tb";
+import { TbPoint, TbSofa, TbBatteryCharging2, TbPlaystationSquare, TbMicrowave, TbWashTumbleDry } from "react-icons/tb";
 
 const categoryIcons = {
   'електроніка': <MdOutlinePhonelink />,
@@ -22,13 +25,29 @@ const subcategoryIcons = {
   'декор': <PiPottedPlantBold />,
   'інструменти': <PiHammerBold />,
   'освітлення': <PiLampPendantBold />,
-  'телефони': <MdOutlinePhonelink />,
+  'побутова техніка': <RiScales2Line />,
+  'посуд': <PiBowlSteamBold />,
+  'приладдя для кухні': <PiForkKnifeBold />,
+  'приладдя для прибирання': <PiBroomFill />,
+  'сантехніка': <PiBathtubBold />,
+  'планшети': <MdTabletMac />,
+  'пристрої для зарядки': <TbBatteryCharging2 />,
+  'для кухні': <TbMicrowave />,
+  'пральні машини': <TbWashTumbleDry />,
+  'аудіотехніка': <AiOutlineAudio />,
+  'ігрові консолі': <TbPlaystationSquare />,
+  "комп'ютери": <FaComputer />,
+  'монітори': <MdOutlineDesktopWindows />,
+  'ноутбуки': <MdOutlineLaptopChromebook />,
+  'телефони': <MdPhoneIphone />,
+  'телевізори': <PiTelevisionSimpleFill />,
+  'фото та відеокамери': <PiVideoCameraBold />,
   'авто': <FaCar />,
   'меблі': <TbSofa />,
   'садові інструменти': <SiGumtree />,
   'чоловічий одяг': <GiClothes />,
-  'жіночий одяг': <GiClothes />,  // Додали іконку для жіночого одягу
-  'взуття': <GiClothes />,       // Додали іконку для взуття
+  'жіночий одяг': <GiClothes />,
+  'взуття': <GiClothes />,
   'художня література': <FaBookDead />,
   'ляльки': <MdOutlineToys />,
   'продукти харчування': <MdEmojiFoodBeverage />,
