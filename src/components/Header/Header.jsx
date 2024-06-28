ab066ba42be631ff864694429c063d02f06b1de7
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-06-28 19:11:50 +0200

    logo updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,8 +14,17 @@ const Header = () => {
       <div className={scss.container}>
         <div className={scss.logoFlex}>
           <NavLink to="/" className={scss.logoLink}>
-            <h1 className={scss.logo}>PlatzUA</h1>
-            <h3>для українців у Німеччині</h3>
+            <div className={scss.logoBackground}>
+              <h1 className={scss.logo}>
+                <span className={scss.logoLetterP}>P</span>
+                <span className={scss.logoLetterL}>l</span>
+                <span className={scss.logoLetterA}>a</span>
+                <span className={scss.logoLetterT}>t</span>
+                <span className={scss.logoLetterZ}>z</span>
+                <span className={scss.logoLetterU}>U</span>
+                <span className={scss.logoLetterA2}>A</span>
+              </h1>
+            </div>
           </NavLink>
         </div>
         <SearchLocation />
