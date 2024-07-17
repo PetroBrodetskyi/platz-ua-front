0b33dd439f2a8e015cc33c70756a760e84fe2939
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-17 23:58:37 +0200

    deploy updated

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RegisterForm from '../../components/Auth/RegisterForm/RegisterForm';
+import ProductList from '../../components/ProductList/ProductList';
 
 const RegisterPage = () => {
     return (
         <div>
             <RegisterForm />
+            <ProductList />
         </div>
     );
 };
