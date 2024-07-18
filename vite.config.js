fa7bf745dbd65077569cebf90331fb128413a681
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-18 16:05:35 +0200

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            src: '/src',
-            components: '/src/components',
-            pages: '/src/pages',
-            assets: '/src/assets',
-            api: '/src/api',
-            helpers: '/src/helpers',
-        },
-    },
-    base: '/platz-ua-front/',
-});
-
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/platz-ua-front/',
-// })
+  plugins: [react()],
+  base: '/platz-ua-front',
+})
