8534df5e860307565e8d78a842ffab5783bd9b41
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-07-18 16:40:43 +0200

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import VitePluginSass from 'vite-plugin-sass'; // Зверніть увагу, що використовується дефолтний імпорт
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginSass() // Використовуйте без деструктуризації, тому що це дефолтний експорт
-  ],
+  plugins: [react()],
   base: '/platz-ua-front/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+})
