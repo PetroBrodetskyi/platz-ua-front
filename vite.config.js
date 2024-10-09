dc32d265ca5852d7455b93e03da827bb903ecbf8
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-09 19:19:05 +0200

    sitemap created

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap'; // Імпорт плагіна
 
 export default defineConfig({
-  plugins: [react()],
-  // base: '/platz-ua-front/',
+  plugins: [
+    react(),
+    sitemap({
+      output: 'public/sitemap.xml', // Шлях для збереження сайт-карти
+      hostname: 'https://platz-ua-front.vercel.app', // Ваше доменне ім'я
+      // Додаткові параметри можна налаштувати тут, наприклад:
+      changefreq: 'daily', // Як часто змінюються сторінки
+      priority: 0.8, // Пріоритет для сторінок
+      exclude: ['/404', '/login'] // Виключення сторінок
+    })
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
