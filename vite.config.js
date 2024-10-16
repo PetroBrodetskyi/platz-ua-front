c8e0a9977921045172cdfe7e264174ddf578c8bf
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-16 23:47:56 +0200

    defineConfig updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      output: 'public/sitemap.xml',
-      hostname: 'https://platzua.com',
-      changefreq: 'daily',
-      priority: 0.8,
-      urls: [
-        { url: '/', changefreq: 'daily', priority: 1.0 },
-        { url: '/how-it-works', changefreq: 'monthly', priority: 0.7 },
-        { url: '/privacy-policy', changefreq: 'monthly', priority: 0.7 },
-        { url: '/create', changefreq: 'monthly', priority: 0.7 }
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'robots.txt',
+          dest: ''
+        }
       ]
     })
   ],
