a4729f48a30644a6e28f130a20d6b8e86abc6421
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-17 12:57:36 +0200

    Home updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,19 @@ import ProductList from '../../components/ProductList/ProductList';
 import CreateAdButton from '../../components/CreateAdButton/CreateAdButton';
 import Logout from '../../components/Logout/Logout';
 import Notification from '../../components/Notification/Notification';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button
+} from '@mui/material';
+import logo from '/public/logo.svg';
 import scss from './Home.module.scss';
 
 const Home = () => {
   const [notification, setNotification] = useState('');
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
   const { user } = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -16,10 +25,20 @@ const Home = () => {
       setNotification(notificationMessage);
       localStorage.removeItem('notification');
     }
+
+    const hasSeenPopup = sessionStorage.getItem('hasSeenPopup');
+    if (!hasSeenPopup) {
+      setIsPopupVisible(true);
+      sessionStorage.setItem('hasSeenPopup', 'true');
+    }
   }, [user]);
 
   const handleCloseNotification = () => {
     setNotification('');
+  };
+
+  const handleClosePopup = () => {
+    setIsPopupVisible(false);
   };
 
   const handleLogout = () => {
@@ -34,6 +53,26 @@ const Home = () => {
           onClose={handleCloseNotification}
         />
       )}
+
+      <Dialog open={isPopupVisible} onClose={handleClosePopup}>
+        <DialogTitle className={scss.logoTitle}>
+          <img src={logo} alt="Логотип" className={scss.logo} />
+          <h4 className={scss.title}>Вітаємо на сайті PlatzUA!</h4>
+        </DialogTitle>
+        <DialogContent className={scss.text}>
+          <b>Сайт ще перебуває у розробці, але ми активно працюємо</b>, щоб
+          якнайшвидше запустити його. Наша мета — створити платформу{' '}
+          <b>для українців у Німеччині</b>, яка стане корисним і надійним
+          ресурсом. Вона надасть можливість зручно купувати, продавати,
+          знаходити друзів, спілкуватися та отримувати підтримку.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClosePopup} color="primary">
+            Закрити
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <ProductList />
       <CreateAdButton />
       {user ? (
