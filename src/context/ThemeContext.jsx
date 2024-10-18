ff1047230db46305663670573a3fdaf3587fe8b3
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-18 20:01:11 +0200

    ThemeContext fixed

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const storedTheme = localStorage.getItem('isDarkMode');
+    return storedTheme === 'true';
+  });
+
+  useEffect(() => {
+    document.body.classList.toggle('darkMode', isDarkMode);
+  }, [isDarkMode]);
 
   const toggleTheme = () => {
     setIsDarkMode((prevMode) => !prevMode);
-    document.body.classList.toggle('darkMode');
+    localStorage.setItem('isDarkMode', !isDarkMode);
   };
 
   return (
