9cfb484ca8d4cc2db5653a7abde09754bc76e689
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-10-19 07:34:59 +0200

    UserProfile updated

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import axios from 'axios';
+import { RiEyeCloseLine } from 'react-icons/ri';
+import { HiOutlineEye } from 'react-icons/hi';
 import scss from './UserProfile.module.scss';
 import Loader from '../Loader';
 import Notification from '../Notification';
@@ -19,13 +21,19 @@ const UserProfile = ({ user }) => {
     instagram: '',
     linkedin: '',
     telegram: '',
-    about: ''
+    about: '',
+    oldPassword: '',
+    newPassword: '',
+    confirmPassword: ''
   });
 
   const [preview, setPreview] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [notification, setNotification] = useState(null);
+  const [showOldPassword, setShowOldPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const token = useSelector((state) => state.auth.token);
 
   useEffect(() => {
@@ -41,7 +49,10 @@ const UserProfile = ({ user }) => {
         instagram: user.instagram || '',
         linkedin: user.linkedin || '',
         telegram: user.telegram || '',
-        about: user.about || ''
+        about: user.about || '',
+        oldPassword: '',
+        newPassword: '',
+        confirmPassword: ''
       });
       setPreview(user.avatarURL || null);
       setLoading(false);
@@ -80,6 +91,14 @@ const UserProfile = ({ user }) => {
     formDataToSend.append('telegram', formData.telegram);
     formDataToSend.append('about', formData.about);
 
+    if (
+      formData.newPassword &&
+      formData.newPassword === formData.confirmPassword
+    ) {
+      formDataToSend.append('oldPassword', formData.oldPassword);
+      formDataToSend.append('newPassword', formData.newPassword);
+    }
+
     try {
       const response = await axios.patch(
         'https://platz-ua-back.vercel.app/api/users/current',
@@ -107,6 +126,18 @@ const UserProfile = ({ user }) => {
 
   const handleAvatarClick = () => {
     document.getElementById('avatarInput').click();
+  };
+
+  const toggleShowOldPassword = () => {
+    setShowOldPassword(!showOldPassword);
+  };
+
+  const toggleShowNewPassword = () => {
+    setShowNewPassword(!showNewPassword);
+  };
+
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   useEffect(() => {
@@ -175,6 +206,7 @@ const UserProfile = ({ user }) => {
                   type="tel"
                   id="phone"
                   name="phone"
+                  placeholder="Номер телефону"
                   value={formData.phone}
                   onChange={handleChange}
                 />
@@ -188,6 +220,80 @@ const UserProfile = ({ user }) => {
                   value={formData.email}
                   onChange={handleChange}
                 />
+              </div>
+              <div className={scss.formGroup}>
+                <label htmlFor="oldPassword">Старий пароль:</label>
+                <div className={scss.inputWrapper}>
+                  <input
+                    type={showOldPassword ? 'text' : 'password'}
+                    id="oldPassword"
+                    name="oldPassword"
+                    placeholder="Введіть поточний пароль"
+                    value={formData.oldPassword}
+                    onChange={handleChange}
+                  />
+                  <button
+                    type="button"
+                    className={scss.eye}
+                    onClick={toggleShowOldPassword}
+                  >
+                    {showOldPassword ? (
+                      <HiOutlineEye color="grey" />
+                    ) : (
+                      <RiEyeCloseLine color="grey" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className={scss.formGroup}>
+                <label htmlFor="newPassword">Новий пароль:</label>
+                <div className={scss.inputWrapper}>
+                  <input
+                    type={showNewPassword ? 'text' : 'password'}
+                    id="newPassword"
+                    name="newPassword"
+                    placeholder="Введіть новий пароль"
+                    value={formData.newPassword}
+                    onChange={handleChange}
+                  />
+                  <button
+                    type="button"
+                    className={scss.eye}
+                    onClick={toggleShowNewPassword}
+                  >
+                    {showNewPassword ? (
+                      <HiOutlineEye color="grey" />
+                    ) : (
+                      <RiEyeCloseLine color="grey" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className={scss.formGroup}>
+                <label htmlFor="confirmPassword">
+                  Підтвердити новий пароль:
+                </label>
+                <div className={scss.inputWrapper}>
+                  <input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    placeholder="Підтвердіть новий пароль"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                  />
+                  <button
+                    type="button"
+                    className={scss.eye}
+                    onClick={toggleShowConfirmPassword}
+                  >
+                    {showConfirmPassword ? (
+                      <HiOutlineEye color="grey" />
+                    ) : (
+                      <RiEyeCloseLine color="grey" />
+                    )}
+                  </button>
+                </div>
               </div>
             </div>
             <AdditionalInfo formData={formData} handleChange={handleChange} />
