54318044e9e92a5a0059694057eea0e34d89f8cd
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-11-09 16:45:31 +0100

    Styles updated

--- src/components/Following/Following.jsx
+++ src/components/Following/Following.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserById } from '../../redux/features/authSlice';
+import { Link } from 'react-router-dom';
 import scss from './Following.module.scss';
 
-const FollowingList = () => {
+const Following = () => {
   const dispatch = useDispatch();
   const followingUsers = useSelector(
     (state) => state.auth.owner?.following || []
@@ -21,8 +22,16 @@ const FollowingList = () => {
       <div className={scss.userList}>
         {followingUsers.map((user) => (
           <div key={user._id} className={scss.userItem}>
-            <img src={user.avatarURL} alt={user.name} className={scss.avatar} />
-            <p className={scss.name}>{user.name}</p>
+            <Link to={`/user/${user._id}`}>
+              <img
+                src={user.avatarURL}
+                alt={user.name}
+                className={scss.avatar}
+              />
+            </Link>
+            <Link to={`/user/${user._id}`} className={scss.name}>
+              {user.name.split(' ')[0]}
+            </Link>
           </div>
         ))}
       </div>
@@ -30,4 +39,4 @@ const FollowingList = () => {
   );
 };
 
-export default FollowingList;
+export default Following;
