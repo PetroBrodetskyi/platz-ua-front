5b7ba79dc6056c202187b041b08b816835223dd1
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-11-17 19:46:35 +0100

    Styles updated

--- src/pages/TermsOfServicePage/TermsOfServicePage.jsx
+++ src/pages/TermsOfServicePage/TermsOfServicePage.jsx
@@ -1,10 +1,15 @@
+import RandomCards from '../../components/RandomCards/RandomCards';
 import TermsOfService from '../../components/TermsOfService';
 import scss from './TermsOfServicePage.module.scss';
 
 const TermsOfServicePage = () => {
   return (
-    <div className={scss.home}>
+    <div className={scss.terms}>
       <TermsOfService />
+      <div className={scss.random}>
+        <h3 className={scss.title}>Вас можуть зацікавити</h3>
+        <RandomCards />
+      </div>
     </div>
   );
 };
