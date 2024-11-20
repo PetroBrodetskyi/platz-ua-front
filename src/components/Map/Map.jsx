25f0fbebb59ad95d93de1584f97759fc70f7042a
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-11-20 14:01:47 +0100

    Map fixed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import scss from './Map.module.scss';
+
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const customIcon = L.icon({
+  iconUrl: markerIcon,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [0, -41],
+  shadowUrl: markerShadow,
+  shadowSize: [41, 41],
+  shadowAnchor: [12, 41]
+});
 
 const Map = ({ latitude, longitude, plz, city }) => {
   return (
@@ -17,8 +31,8 @@ const Map = ({ latitude, longitude, plz, city }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[latitude, longitude]}>
-        <Popup>
+      <Marker position={[latitude, longitude]} icon={customIcon}>
+        <Popup className={scss.popup}>
           {plz} {city}
         </Popup>
       </Marker>
