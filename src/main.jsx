0a046f3b1f14e74e6c5095b7b364e25cb4899284
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-11-22 22:55:48 +0100

    helmet updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 import store from './redux/store.js';
 import './styles/index.scss';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
